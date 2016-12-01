25372b644df9077042bea0f6462c1f3ed20d9612
k8mil <kamil.wysocki93@gmail.com>
2016-12-01 11:29:42 +0100

    Initial commit

---
+++ Droid/CustomPickerRenderer.cs
@@ -0,0 +1,34 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using JPC.BindablePicker;
+using TestApp.Droid;
+
+
+[assembly: ExportRenderer(typeof(BindablePicker), typeof(BindablePickerRenderer))]
+
+namespace TestApp.Droid
+{
+	public class BindablePickerRenderer : PickerRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+		{
+			base.OnElementChanged(e);
+
+			var picker = e.NewElement;
+			BindablePicker bp = (BindablePicker)this.Element;
+			if (this.Control != null)
+			{
+				var pickerStyle = new Style(typeof(BindablePicker))
+				{
+					Setters =
+					{
+						new Setter { Property = VisualElement.BackgroundColorProperty, Value = Color.Red }
+
+					}
+				};
+				picker.Style = pickerStyle;
+			}
+		}
+	}
+}
+
---
+++ TestApp/TestAppPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace TestApp
+{
+	public partial class TestAppPage : ContentPage
+	{
+		public TestAppPage()
+		{
+			InitializeComponent();
+			List<string> list = new List<string>(new string[] { "ABC", "CBA", "EDF" });
+			this.MyPicker.ItemsSource = list;
+
+		}
+	}
+}
